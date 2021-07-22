2ad70d820896ae020e0d57dcde039b4e33aaa29d
Anand Upadhyay <87755960+anandupa2001@users.noreply.github.com>
2021-07-22 14:52:40 +0530

    Add files via upload

---
+++ Project2 task2 (1).py
@@ -0,0 +1,12 @@
+# Python program to print positive Numbers in a range
+ 
+# range of numbers
+list1 = [12, -7, 5, 64, -14]
+ 
+# iterating each number in range
+for num in list1:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
---
+++ Project2 task2 (2).py
@@ -0,0 +1,12 @@
+# Python program to print positive Numbers in a range
+ 
+# range of numbers
+list2 = [12, 14, -95, 3]
+ 
+# iterating each number in range
+for num in list2:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
